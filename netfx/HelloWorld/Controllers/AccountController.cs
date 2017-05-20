8cdcbd59f4fb717e64ec87c11c210940a71823b9
Brian Engen <engenb@gmail.com>
2017-05-20 17:46:07 -0500

    added necessary stuff for Titan Identity

---
+++ netfx/HelloWorld/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        // GET: /Account/Login
+        public ActionResult Login(string returnUrl)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        // POST: /Account/LogOff
+        [HttpPost]
+        public ActionResult LogOff()
+        {
+            return Redirect("https://test.redarrow.io/auth/logout");
+        }
+    }
+}
---
+++ netfx/HelloWorld/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+
+[assembly: OwinStartup(typeof(HelloWorld.Startup))]
+namespace HelloWorld
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app
+                .UseCookieAuthentication(new CookieAuthenticationOptions
+                {
+                    AuthenticationType = CookieAuthenticationDefaults.AuthenticationType
+                })
+                .UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+                {
+                    SignInAsAuthenticationType = CookieAuthenticationDefaults.AuthenticationType,
+
+                    ClientId = "0620bd8e-2790-4e68-bff7-232e77701866",
+                    Authority = "https://test.redarrow.io/auth",
+                    RedirectUri = "http://localhost:8080/auth-callback",
+
+                    ResponseType = "code id_token token",
+                    Scope = "openid profile email"
+                });
+        }
+    }
+}
